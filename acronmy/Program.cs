58fb5c9f4a2bade184d5d57e291796ce59b7f098
yemisrach Mekuria <meserlengereh@gmail.com>
2020-02-28 11:07:00 -0500

    adding acromny working code

--- acronmy/Program.cs
+++ acronmy/Program.cs
@@ -4,9 +4,42 @@ namespace acronmy
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+        static String firstLetterWord(String str)  
+    {  
+        String result = "";  
+  
+        // Traverse the string.  
+        bool v = true;  
+        for (int i = 0; i < str.Length; i++)  
+        {  
+            // If it is space, set v as true.  
+            if (str[i] == ' ')  
+            {  
+                v = true;  
+            }  
+              
+            // Else check if v is true or not.  
+            // If true, copy character in output  
+            // string and set v as false.  
+            else if (str[i] != ' ' && v == true)  
+            {  
+                result += (str[i]);  
+                v = false;  
+            }  
+        }  
+        return result;  
+    }  
+  
+    // Driver code  
+    public static void Main()  
+    {  
+        // String str = "geeks for geeks";  
+        // Console.WriteLine(firstLetterWord(str));  
+        Console.WriteLine("write any sentense?");
+        string str=Console.ReadLine();  
+       
+        firstLetterWord(str);
+         Console.WriteLine(firstLetterWord(str));
+    }  
+}  
 }
