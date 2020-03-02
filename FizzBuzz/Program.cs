9fb7460b97e5ce5322e597c8bf5f2d0644b40fd8
yemisrach Mekuria <meserlengereh@gmail.com>
2020-03-02 15:22:51 -0500

    final min

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -7,31 +7,23 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
              {  
-        int n = 100; 
+        int n = 1000; 
   
        
         for (int i = 1; i <= n; i++)                              
         { 
-            // number divisible by 5,  
-            // print 'Buzz' in place 
-            // of the number 
+           
             if (i % 15 == 0)  
                 Console.Write("FizzBuzz" + " ");  
   
-            // number divisible by 3,  
-            // print 'Fizz' in place  
-            // of the number 
+           
             else if (i % 3 == 0)      
                 Console.Write("Fizz" + " ");  
   
-            // number divisible by  
-            // 15(divisible by both  
-            // 3 & 5), print 'FizzBuzz' 
-            // in place of the number 
             else if (i % 5 == 0)                                              
                 Console.Write("Buzz" + " ");  
               
-            // print the numbers  
+            
             else
                 Console.Write(i + " ");                      
         } 
@@ -43,5 +35,6 @@ namespace FizzBuzz
     }
  
 }
-//i got 
-//27 fizz,13 buzz,6 fizzbuzz
+//267 fizz
+//134 buzz 
+//67 fizzbuzz
