bb79fe41a0d46039103d3d3e30ebc2fcf98e434f
yemisrach Mekuria <meserlengereh@gmail.com>
2020-03-02 14:45:54 -0500

    adding buzzfizz code

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             {  
+        int n = 100; 
+  
+        // loop for 100 times 
+        for (int i = 1; i <= n; i++)                              
+        { 
+            // number divisible by 5,  
+            // print 'Buzz' in place 
+            // of the number 
+            if (i % 15 == 0)  
+                Console.Write("FizzBuzz" + " ");  
+  
+            // number divisible by 3,  
+            // print 'Fizz' in place  
+            // of the number 
+            else if (i % 3 == 0)      
+                Console.Write("Fizz" + " ");  
+  
+            // number divisible by  
+            // 15(divisible by both  
+            // 3 & 5), print 'FizzBuzz' 
+            // in place of the number 
+            else if (i % 5 == 0)                                              
+                Console.Write("Buzz" + " ");  
+              
+            // print the numbers  
+            else
+                Console.Write(i + " ");                      
+        } 
+    } 
+} 
+  
+// This code is contributed 
+// by shiv_bhakt. 
+
+    }
+ 
+}
