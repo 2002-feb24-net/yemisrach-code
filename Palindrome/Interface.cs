5c1c09f2aeb31740d09fca8a1042320f9ce2d571
yemisrach Mekuria <meserlengereh@gmail.com>
2020-03-16 12:32:47 -0400

    check pal

---
+++ Palindrome/Interface.cs
@@ -0,0 +1,23 @@
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndInterface
+{
+    interface Interface1
+    { 
+        
+  int CheckPalindrome(); { get; set; }
+  int  StringPalindrome();{get ;set;}
+
+
+         CheckPalindrome();
+         StringPalindrome();
+    }
+}
+
+
+
+
+ 
